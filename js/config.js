405b3c1f3df9fa86fa06812b53084ef0089504b1
Manishkatheeth-aws <mvk230000@utdallas.edu>
2024-12-07 08:38:49 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_VEpbk0DO3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1415pibe6o6l37gi6rs0qlijvo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
